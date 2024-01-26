05f50cb69dd4995f80935a83f03d9dd98f5b24d6
Mcmxcvi25 <tema.polivoda@gmail.com>
2024-01-26 07:51:54 +0200

    script fixeed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // в эту константу помещаем URL развёрнутого веб-приложения Google Apps Script
 // ВНИМАНИЕ! Это должен быть адрес ВАШЕГО РАЗВЕРНУТОГО ПРИЛОЖЕНИЯ
 // ТЕКУЩИЙ URL_APP приведён для примера
-const URL_APP = "https://script.google.com/macros/s/AKfycbygJx2puASNOkDwzuGTvfe_bB4etFMTTk0J4wsbzekfMJH4hxcySN-dvtiqQMYXV6eQrg/exec";
+const URL_APP = "https://script.google.com/macros/s/AKfycbyrjloEt7zjJVoR5EYXI1g6LfKUumhX4kSJu-xkBJdU5hoU5h2LvVBh4jJy2ItwZAGV_Q/exec";
 
 // находим форму в документе
 const form = document.querySelector("#form");
@@ -9,41 +9,20 @@ const form = document.querySelector("#form");
 // указываем адрес отправки формы (нужно только в начале примера)
 form.action = URL_APP;
 
-// вспомогательная функция проверки заполненности формы
-// function isFilled(details) {
-//   const { name, email, phone, rule } = details;
-//   if (!name) return false;
-//   if (!email) return false;
-//   if (!phone) return false;
-//   if (!rule) return false;
-//   return true;
-// }
-
 // навешиваем обработчик на отправку формы
 form.addEventListener("submit", async (ev) => {
   // отменяем действие по умолчанию
   ev.preventDefault();
 
   // получаем ссылки на элементы формы
-  // const name = document.querySelector("[name=name]");
-  // const email = document.querySelector("[name=email]");
-  // const phone = document.querySelector("[name=phone]");
   const message = document.querySelector("[name=message]");
-  // const rule = document.querySelector("[name=rule]");
   const question = document.querySelector("[name=question]:checked");
 
   // собираем данные из элементов формы
   let details = {
-    // name: name.value.trim(),
-    // email: email.value.trim(),
-    // phone: phone.value.trim(),
     message: message.value.trim(),
-    // rule: rule.value,
     question: question.value,
   };
-
-  // если поля не заполнены - прекращаем обработку
-  // if (!isFilled(details)) return;
 
   // подготавливаем данные для отправки
   let formBody = [];
@@ -70,21 +49,11 @@ form.addEventListener("submit", async (ev) => {
   // .then((res) => console.log(res));
 
   if (result.type === "success") {
-    // name.value = '';
-    // email.value = '';
-    // phone.value = '';
     message.value = "";
     question.value = "";
-    alert("Дякуємо, Вас буде перенаправлено до нашого сайту через 5 секунд");
+    alert("Дякуємо за оцінку!");
   }
   if (result.type === "error") {
     alert(`Помилка( ${result.errors}`);
   }
 });
-
-// function updatePhoneNumber(input) {
-//   // Если введенное значение не начинается с "+380", добавляем "+380" в начало
-//   if (!input.value.startsWith("+380")) {
-//     input.value = "+380" + input.value;
-//   }
-// }
